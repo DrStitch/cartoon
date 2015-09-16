792aac10ecd84ab5fc854a5fb5891420166afc8c
DrStitch <467738357@qq.com>
2015-09-16 19:35:21 +0800

    ClearFiles

--- cartoon/extensions.py
+++ cartoon/extensions.py
@@ -3,11 +3,14 @@ __author__ = 'stitch'
 from scrapy import signals
 from scrapy.exceptions import NotConfigured
 import tarfile
+import shutil
+import os
 from contextlib import closing
 
 class TarFolder(object):
     def __init__(self):
         self.folder = 'pic/full'
+        self.filename = 'complete.tar.gz'
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -18,7 +21,7 @@ class TarFolder(object):
         return ext
 
     def tar_folder(self, spider):
-        with closing(tarfile.TarFile.gzopen('complete.tar.gz', 'w')) as out:
+        with closing(tarfile.TarFile.gzopen(self.filename, 'w')) as out:
             out.add(self.folder)
 '''
 class SendEmail(object):
@@ -26,3 +29,20 @@ class SendEmail(object):
         self.frm = '467738357@qq.com'
         self.to = '467738357@qq.com'
 '''
+
+class ClearFile(object):
+    def __init__(self):
+        self.folder = 'pic/full'
+        self.filename = 'complete.tar.gz'
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        if not crawler.settings.getbool('MYEXT_ENABLED'):
+            raise NotConfigured
+        ext = cls()
+        crawler.signals.connect(ext.clear_file, signal=signals.spider_closed)
+        return ext
+
+    def clear_file(self, spider):
+        shutil.rmtree(self.folder)
+        os.remove(self.filename)
